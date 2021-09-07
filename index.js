c566153a713d90f70e6bc423609647c6dcb5a445
Kyle Samuel <ksamuel92@gmail.com>
2021-09-07 11:36:33 -0400

    commit tree d94a34f7ce4ed09029846c6764f5d4f50afcbea6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+  const lowerCaseName = name.toLowerCase()
+  return drivers.filter(driver => driver.toLowerCase() === lowerCaseName)
+}
+
+function fuzzyMatch(drivers, testString) {
+  return drivers.filter( possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  )
+}
+
+function matchName(drivers, testString) {
+  return drivers.filter(driver => driver["name"].toLowerCase() === testString.toLowerCase())
+}
